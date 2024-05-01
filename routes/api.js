d56b99e597daa1702650772621ceddabfbc353f8
Keegan Micu <LemonTag73@gmail.com>
2024-05-01 15:42:53 -0400

    got ride of the index.html and add code to html and api

---
+++ routes/api.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const router = require('express').Router();
+
+// GET Route for feedback page
+router.get('/notes', (req, res) =>
+{
+
+})
+
+
+// * means all paths it will check /notes first
+// GET Route for homepage
+router.post('/notes', (req, res) =>
+{
+
+})
+
+
+module.exports = router
