8df56ef565be6fa2593d87540f582b846192ff89
Keegan Micu <LemonTag73@gmail.com>
2024-05-02 20:38:01 -0400

    my cards now added and saved

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const router = require('express').Router();
 const store = require('./store')
+const help = require('./help')
 
 // GET Route for feedback page
 router.get('/notes', (req, res) => {
@@ -11,6 +12,28 @@ router.get('/notes', (req, res) => {
     })
 })
 
+// POST Route for a new UX/UI tip
+router.post('/notes', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: help(),
+      };
 
+      
+    store.addNote(newNote)
+    .then((note) => 
+        res.json(note)
+    ) .catch(() => {
+        res.status(500).json(err);
+    })
+    }
 
+})
+  
 module.exports = router;
--- routes/store.js
+++ routes/store.js
@@ -1,27 +1,27 @@
 const fs = require('fs');
-const util =require('util');
+const util = require('util');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-class Store{
+class Store {
     //this is reading from the file
     read() {
-        return readFileAsync('db/db.json', 'utf8');
+        return readFileAsync('./db/db.json', 'utf8');
     }
 
     //we are making sure we are stringifying the object we are writing
-    write(note){
-        return writeFileAsync('db/db.json', JSON.stringify(note));
+    write(note) {
+        return writeFileAsync('./db/db.json', JSON.stringify(note));
     }
 
-    getNotes(){
+    getNotes() {
         //return the notes
         return this.read().then((notes) => {
-            let parsedNotes; 
-            try{
+            let parsedNotes;
+            try {
                 parsedNotes = [].concat(JSON.parse(notes));
-            }catch(err) {
+            } catch (err) {
                 console.log(err)
                 parseNotes = []
             }
@@ -30,13 +30,28 @@ class Store{
     }
 
     //make is a single then more notes
-    addNote(note){
+    addNote(note) {
+        return this.getNotes()
+        .then((parsedNotes) => {
+            parsedNotes.push(note);
+            console.log(parsedNotes)
+            return this.write(parsedNotes);
+        })
+        .then(() => {
+            console.log('Note added successfully!');
+        })
+        .catch(error => {
+            console.error('Error adding note:', error);
+        });
+
 
     }
 
-    removeNote(id){
 
-    }
+removeNote(id)  {
+
+}
+
 }
 
 
